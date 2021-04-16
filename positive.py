a822bc68225a8605d6a0cfa8839faad941c7aa38
Graham Breed <gbreed@gmail.com>
2021-04-16 08:53:32 +0100

    Support more general positive tunings

--- positive.py
+++ positive.py
@@ -2,10 +2,10 @@
 
 # These really use the 2.3.10.14... limits
 TUNINGS = [
-    ('11-limit TE', 'te_11', 1200.3191, 497.6227),
-    ('11-limit POTE', 'pote_11', 1200.0000, 497.4904),
-    ('13-limit TE', 'te_13', 1200.4603, 497.4976),
-    ('13-limit POTE', 'pote_13', 1200.0000, 497.3069),
+    ('Andromeda', 'biased 11-limit TE', 'te_11', 1200.3191, 497.6227),
+    ('Andromeda', 'biased 11-limit POTE', 'pote_11', 1200.0000, 497.4904),
+    ('Andromeda', 'biased 13-limit TE', 'te_13', 1200.4603, 497.4976),
+    ('Andromeda', 'biased 13-limit POTE', 'pote_13', 1200.0000, 497.3069),
     ]
 OCTAVE, FOURTH = 53, 22
 
@@ -35,42 +35,64 @@ def get_scale(bottom, top, octave_cents, fourth_cents):
 #   19  20  21  22  23
 
 if __name__ == '__main__':
-    for (tuning, label, octave, fourth) in TUNINGS:
-        filename = 'andromeda_29_f_' + label + '.scl'
+    for (tclass, tuning, label, octave, fourth) in TUNINGS:
+        filename = '{}_29_f_{}.scl'.format(
+                tclass.lower(), label)
         with open(filename, 'w') as out:
             pitches = get_scale(-5, 23, octave, fourth)
             out.write("! " + filename + "\n")
             out.write("!\n")
             out.write(
-                    "andromeda on C with fourths from Pythagorean F tuned to biased "
+                    tclass
+                    + " on C with fourths from Pythagorean F tuned to "
                     + tuning + "\n")
             out.write("%i\n" % len(pitches))
             out.write("!\n")
             for pitch in pitches:
                 out.write("%.3f\n" % pitch)
 
-    for (tuning, label, octave, fourth) in TUNINGS:
-        filename = 'andromeda_41_d_' + label + '.scl'
+    for (tclass, tuning, label, octave, fourth) in TUNINGS:
+        filename = '{}_29_d_{}.scl'.format(
+                tclass.lower(), label)
+        with open(filename, 'w') as out:
+            pitches = get_scale(-16, 12, octave, fourth)
+            out.write("! " + filename + "\n")
+            out.write("!\n")
+            out.write(
+                    tclass
+                    + " on C with fourths symmetrically from D tuned to "
+                    + tuning + "\n")
+            out.write("%i\n" % len(pitches))
+            out.write("!\n")
+            for pitch in pitches:
+                out.write("%.3f\n" % pitch)
+
+    for (tclass, tuning, label, octave, fourth) in TUNINGS:
+        filename = '{}_41_d_{}.scl'.format(
+                tclass.lower(), label)
         with open(filename, 'w') as out:
             pitches = get_scale(-22, 18, octave, fourth)
             out.write("! " + filename + "\n")
             out.write("!\n")
             out.write(
-                    "andromeda on C with fourths symmetrically from D tuned to biased "
+                    tclass
+                    + " on C with fourths symmetrically from D tuned to "
                     + tuning + "\n")
             out.write("%i\n" % len(pitches))
             out.write("!\n")
             for pitch in pitches:
                 out.write("%.3f\n" % pitch)
 
-    for (tuning, label, octave, fourth) in TUNINGS:
-        filename = 'andromeda_53_d_' + label + '.scl'
+    for (tclass, tuning, label, octave, fourth) in TUNINGS:
+        filename = '{}_53_d_{}.scl'.format(
+                tclass.lower(), label)
         with open(filename, 'w') as out:
             pitches = get_scale(-28, 24, octave, fourth)
             out.write("! " + filename + "\n")
             out.write("!\n")
             out.write(
-                    "andromeda on C with fourths symmetrically from D tuned to biased "
+                    tclass
+                    + " on C with fourths symmetrically from D tuned to "
                     + tuning + "\n")
             out.write("%i\n" % len(pitches))
             out.write("!\n")
