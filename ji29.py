6610e17c7b66891c42dd27058cb3c96125a15a9f
Graham Breed <gbreed@gmail.com>
2021-05-30 08:56:08 +0100

    Choose chords before diatonics

--- ji29.py
+++ ji29.py
@@ -5,12 +5,33 @@ from math import log2
 
 octave = 29
 diatonic = '1/1 9/8 81/64 4/3 3/2 27/16 16/9'
+chord = '1/1 9/8 5/4 11/8 3/2 13/8 7/4 15/8'
 comma = Fraction(352, 351)
 
 gamut = [None] * octave
 
 def scale_steps(pitch):
     return int(round(log2(pitch) * octave))
+
+for pitch in map(Fraction, chord.split()):
+    pitch_steps = scale_steps(pitch)
+    for tonic in map(Fraction, '1/1 9/8 4/3 3/2 27/16'.split()):
+        new_pitch = pitch * tonic
+        new_steps = pitch_steps + scale_steps(tonic)
+        while new_steps < 0:
+            new_steps += octave
+            new_pitch *= 2
+        while new_steps >= octave:
+            new_steps -= octave
+            new_pitch /= 2
+        old_pitch = gamut[new_steps]
+        if old_pitch is None:
+            gamut[new_steps] = new_pitch
+        elif new_pitch != old_pitch:
+            print("Discrepancy: {}, {}".format(new_pitch, old_pitch))
+            if new_pitch.denominator < old_pitch.denominator:
+                gamut[new_steps] = new_pitch
+
 
 for pitch in map(Fraction, diatonic.split()):
     pitch_steps = scale_steps(pitch)
@@ -23,16 +44,9 @@ for pitch in map(Fraction, diatonic.split()):
         while new_steps >= octave:
             new_steps -= octave
             new_pitch /= 2
-        alternative_pitch = new_pitch * comma
-        if (tonic.numerator == 13
-                and alternative_pitch.denominator < new_pitch.denominator):
-            new_pitch = alternative_pitch
-        assert gamut[new_steps] is None
-        gamut[new_steps] = new_pitch
-
-b_natural = Fraction(243, 128)
-assert gamut[scale_steps(b_natural)] is None
-gamut[scale_steps(b_natural)] = b_natural
+        if gamut[new_steps] is None:
+            print("New pitch needed: {}".format(new_pitch))
+            gamut[new_steps] = new_pitch
 
 gamut.sort()
 
